19d2ef40d4b80e7182cdd76227c1403c1365d1be
CharlesHahn <3447180435@qq.com>
2022-03-06 15:15:49 +0800

    add call_functions to NDX

---
+++ DuIvyTools/MDP.py
@@ -0,0 +1,8 @@
+""" NDX module is part of DuIvyTools library, which is a tool for analysis and visualization of GROMACS result files. This module is written by CharlesHahn.
+
+This module is designed to process and deal with ndx (GMX index) file. 
+
+This NDX module contains:
+    - NDX class
+
+This file is provided to you by GPLv2 license."""
