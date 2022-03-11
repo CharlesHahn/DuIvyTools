662312df8d16b541aff9f9cbf2a6af49815d1ce5
CharlesHahn <3447180435@qq.com>
2022-03-11 19:41:37 +0800

    >> is not good, use ~~ for instead

--- DuIvyTools/XVG.py
+++ DuIvyTools/XVG.py
@@ -1404,11 +1404,11 @@ def xvg_call_functions(arguments: list = None):
     if args.legend_list != None:
         if "," in "".join(args.legend_list):
             for legends in args.legend_list:
-                ## >> is used to reprensent space
-                legend_list.append([l.replace(">>", " ") for l in legends.split(",")])
+                ## ~~ is used to reprensent space
+                legend_list.append([l.replace("~~", " ") for l in legends.split(",")])
         else:
-            ## >> is used to reprensent space
-            legend_list = [l.replace(">>", " ") for l in args.legend_list]
+            ## ~~ Is used to reprensent space
+            legend_list = [l.replace("~~", " ") for l in args.legend_list]
     firstfile = (xvgfiles[0], xvgfiles[0][0])[isinstance(xvgfiles[0], list)]
 
     ## call functions
