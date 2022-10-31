227829fb3dc46b78291a6a17c3ba7785b90f0c26
CharlesHahn <3447180435@qq.com>
2022-10-31 20:11:08 +0800

    final test of docs

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,8 @@ templates_path = ['templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh_CN'
+# language = 'zh_CN'
+language = "en_US"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
