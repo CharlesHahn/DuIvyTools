8a99851467a7b760a5b4f0f5696de3c2f174dad7
CharlesHahn <3447180435@qq.com>
2026-08-18 22:30:19 +0800

    add docs_en

--- docs/conf.py
+++ docs/conf.py
@@ -39,8 +39,7 @@ templates_path = ['templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-# language = 'zh_CN'
-language = "en_US"
+language = "zh_CN"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
